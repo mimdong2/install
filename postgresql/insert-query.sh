0fba1bf22aee0f4b9ae80f960943121aeb7b23c2
SUN MIN JUNG <118809875+mimdong2@users.noreply.github.com>
2023-02-15 08:21:06 +0900

    Update insert-query.sh

--- postgresql/insert-query.sh
+++ postgresql/insert-query.sh
@@ -3,5 +3,5 @@
 echo '>>>>> [PostgreSQL] pitr_test 테이블 생성'
 psql -U postgres -d postgres -c 'CREATE TABLE IF NOT EXISTS pitr_test (id SERIAL PRIMARY KEY, create_date timestamp)'
 
-echo '>>>>> [PostgreSQL] insert query를 위한 쉘 스크립트 생성'
+echo '>>>>> [PostgreSQL] insert query를 위한 쉘 스크립트 생성(/var/lib/postgresql/15/main/pitr_insert.sh)'
 echo "psql -U postgres -d postgres -c 'INSERT INTO pitr_test (create_date) values (now())'" > /var/lib/postgresql/15/main/pitr_insert.sh
